25b051d8994fadc24790429ba415decc11a47306
Naohide Sano <namachapanda2005@gmail.com>
2022-03-08 18:27:56 +0900

    catch up w/ library update

--- src/main/java/vavi/nio/file/truevfs/TrueVfsFileAttributesFactory.java
+++ src/main/java/vavi/nio/file/truevfs/TrueVfsFileAttributesFactory.java
@@ -6,7 +6,7 @@
 
 package vavi.nio.file.truevfs;
 
-import com.github.fge.filesystem.attributes.FileAttributesFactory;
+import com.github.fge.filesystem.driver.ExtendedFileSystemDriverBase.ExtendsdFileAttributesFactory;
 
 import net.java.truevfs.access.TFile;
 
@@ -17,7 +17,7 @@ import net.java.truevfs.access.TFile;
  * @author <a href="mailto:umjammer@gmail.com">Naohide Sano</a> (umjammer)
  * @version 0.00 2021/11/30 umjammer initial version <br>
  */
-public final class TrueVfsFileAttributesFactory extends FileAttributesFactory {
+public final class TrueVfsFileAttributesFactory extends ExtendsdFileAttributesFactory {
 
     public TrueVfsFileAttributesFactory() {
         setMetadataClass(TFile.class);
